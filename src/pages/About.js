ac21b7d39730e059e5fa9534bb6a944acd897988
Anass <anass59000@gmail.com>
2022-08-08 12:14:07 +0200

    creation des pages

---
+++ src/pages/About.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+             <br/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, alias 
+                assumenda hic adipisci modi est omnis a voluptas blanditiis tempore repellendus 
+                dolor iusto beatae nisi officiis quibusdam corrupti incidunt quo placeat vel laudantium 
+                pariatur error corporis eveniet. Placeat exercitationem voluptate soluta amet dolore nam
+                 recusandae blanditiis quasi, aspernatur suscipit iusto omnis, itaque perferendis quos numquam commodi,
+                  doloremque repellendus. Cupiditate impedit soluta nesciunt laborum culpa esse, maxime neque tenetur
+                   consequatur quae quia eum, corrupti, placeat voluptate alias sequi in inventore numquam ducimus
+                    voluptatum quod optio sed. Fuga incidunt sapiente sint reiciendis facere nam atque aspernatur 
+                    nihil neque, nulla adipisci quia enim doloribus totam recusandae quas voluptatibus? Illo nisi nihil 
+                    itaque aspernatur incidunt alias eligendi, quam possimus, vero quis quidem maxime sed. Nam, corporis 
+                    eum eligendi, quos totam ut fugiat provident dolorem earum illo sit, possimus reiciendis quisquam 
+                    laboriosam. Totam doloremque aperiam unde ex amet minima omnis in eum quis maiores, soluta magnam ut 
+                    iste. Qui at ipsa numquam dolores corporis suscipit, odit tempora culpa similique architecto, dicta
+                     sint excepturi perferendis illo eaque laborum quis sunt delectus praesentium esse, itaque
+                      exercitationem officia repudiandae? Optio quisquam similique quia expedita molestiae vitae, 
+                     quas, doloribus voluptatem fugit veritatis tempora quaerat a consequatur dicta consectetur
+              </p>           
+             <br/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, alias 
+                assumenda hic adipisci modi est omnis a voluptas blanditiis tempore repellendus 
+                dolor iusto beatae nisi officiis quibusdam corrupti incidunt quo placeat vel laudantium 
+                pariatur error corporis eveniet. Placeat exercitationem voluptate soluta amet dolore nam
+                 recusandae blanditiis quasi, aspernatur suscipit iusto omnis, itaque perferendis quos numquam commodi,
+                  doloremque repellendus. Cupiditate impedit soluta nesciunt laborum culpa esse, maxime neque tenetur
+                   consequatur quae quia eum, corrupti, placeat voluptate alias sequi in inventore numquam ducimus
+                    voluptatum quod optio sed. Fuga incidunt sapiente sint reiciendis facere nam atque aspernatur 
+                    nihil neque, nulla adipisci quia enim doloribus totam recusandae quas voluptatibus? Illo nisi nihil 
+                    itaque aspernatur incidunt alias eligendi, quam possimus, vero quis quidem maxime sed. Nam, corporis 
+                    eum eligendi, quos totam ut fugiat provident dolorem earum illo sit, possimus reiciendis quisquam 
+                    laboriosam. Totam doloremque aperiam unde ex amet minima omnis in eum quis maiores, soluta magnam ut 
+                    iste. Qui at ipsa numquam dolores corporis suscipit, odit tempora culpa similique architecto, dicta
+                     sint excepturi perferendis illo eaque laborum quis sunt delectus praesentium esse, itaque
+                      exercitationem officia repudiandae? Optio quisquam similique quia expedita molestiae vitae, 
+                     quas, doloribus voluptatem fugit veritatis tempora quaerat a consequatur dicta consectetur
+              </p>           
+        </div>
+    );
+};
+
+export default About;
